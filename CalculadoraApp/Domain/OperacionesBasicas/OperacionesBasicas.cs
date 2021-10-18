2eb8287d15638e27c25805a7cc9992f0913baf51
josebaltodano <89146748+josebaltodano@users.noreply.github.com>
2021-10-17 18:45:25 -0600

    Add files via upload

---
+++ CalculadoraApp/Domain/OperacionesBasicas/OperacionesBasicas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.OperacionesBasicas
+{
+   public   class   OperacionesBasicas
+    {
+     
+
+        public double [] suma { get; set; }
+
+        public double [] resta { get; set; }
+        public double [] division { get; set; }
+        public double [] Multiplicaion { get; set; }
+        public double [] Elevacion { get; set; }
+    
+    }
+}
---
+++ CalculadoraApp/Infraestructura/Metodos/MetodoDeOperaciones.cs
@@ -0,0 +1,61 @@
+﻿using Domain.OperacionesBasicas;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructura.Metodos
+{
+  public    class MetodoDeOperaciones
+    {
+        #region MetodosBasicos
+
+        private OperacionesBasicas operacionesBasicas;
+        public double  Getsuma()
+        {
+            double o =0;
+            for (int i =0; i < operacionesBasicas.suma .Length ; i++)
+            {
+                o = o + operacionesBasicas.suma[i];
+            }
+            return o;
+        }
+        public double GetResta()
+        { double x = 0;
+            for(int I =0; I < operacionesBasicas.resta.Length; I++)
+            {
+                x = x - operacionesBasicas.resta[I];
+            }
+            return x;
+        }
+        public double GetMultiplicacion() {
+
+            double emp = 0;
+            for (int i=0; i < operacionesBasicas.Multiplicaion.Length; i++)
+            {
+                emp = emp * operacionesBasicas.Multiplicaion[i];
+            }
+            return emp;
+        }
+         
+        public double GetDivision()
+        {
+            double tem = 0;
+            for (int i =0; i< operacionesBasicas.division.Length; i++)
+            {
+                tem = tem / operacionesBasicas.division[i];
+            }
+            return tem;
+        }
+
+        public double GetElevarUnNumero()
+        {
+            double elv = 0;
+            for(int I=0; I < operacionesBasicas.Elevacion.Length; I++)
+            {
+                elv = Math.Pow(elv, operacionesBasicas.Elevacion[I]);
+            }
+            return 0;
+        }
+        #endregion
+    }
+}
